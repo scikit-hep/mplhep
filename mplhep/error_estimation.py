c8dd9654447bffa201f446b1bf5b0eb826830efc
andrzejnovak <novak5andrzej@gmail.com>
2019-11-18 17:55:24 -0600

    FIX

---
+++ mplhep/error_estimation.py
@@ -0,0 +1,50 @@
+import warnings
+
+import scipy.stats
+import numpy as np
+
+
+_coverage1sd = scipy.stats.norm.cdf(1) - scipy.stats.norm.cdf(-1)
+
+
+def poisson_interval(sumw, sumw2, coverage=_coverage1sd):
+    """Frequentist coverage interval for Poisson-distributed observations
+    Parameters
+    ----------
+        sumw : numpy.ndarray
+            Sum of weights vector
+        sumw2 : numpy.ndarray
+            Sum weights squared vector
+        coverage : float, optional
+            Central coverage interval, defaults to 68%
+    Calculates the so-called 'Garwood' interval,
+    c.f. https://www.ine.pt/revstat/pdf/rs120203.pdf or
+    http://ms.mcmaster.ca/peter/s743/poissonalpha.html
+    For weighted data, this approximates the observed count by ``sumw**2/sumw2``, which
+    effectively scales the unweighted poisson interval by the average weight.
+    This may not be the optimal solution: see https://arxiv.org/pdf/1309.1287.pdf for a
+    proper treatment. When a bin is zero, the scale of the nearest nonzero bin is
+    substituted to scale the nominal upper bound.
+    If all bins zero, a warning is generated and interval is set to ``sumw``.
+    # Taken from Coffea
+    """
+    scale = np.empty_like(sumw)
+    scale[sumw != 0] = sumw2[sumw != 0] / sumw[sumw != 0]
+    if np.sum(sumw == 0) > 0:
+        missing = np.where(sumw == 0)
+        available = np.nonzero(sumw)
+        if len(available[0]) == 0:
+            warnings.warn("All sumw are zero!  Cannot compute meaningful error bars",
+                          RuntimeWarning)
+            return np.vstack([sumw, sumw])
+        nearest = sum([
+            np.subtract.outer(d, d0)**2 for d, d0 in zip(available, missing)
+        ]).argmin(axis=0)
+        argnearest = tuple(dim[nearest] for dim in available)
+        scale[missing] = scale[argnearest]
+    counts = sumw / scale
+    lo = scale * scipy.stats.chi2.ppf((1 - coverage) / 2, 2 * counts) / 2.
+    hi = scale * scipy.stats.chi2.ppf((1 + coverage) / 2, 2 * (counts + 1)) / 2.
+    interval = np.array([lo, hi])
+    interval[interval == np.nan] = 0.  # chi2.ppf produces nan for counts=0
+    return interval
