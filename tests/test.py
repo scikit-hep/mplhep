b0bf6a8d4f65d09c9d46eb2bd822a5a03a426ea8
andrzejnovak <novak5andrzej@gmail.com>
2019-11-21 10:12:12 -0600

    rollback

--- tests/test.py
+++ tests/test.py
@@ -101,6 +101,6 @@ def test_histplot_stack():
 @pytest.mark.mpl_image_compare(style='default', remove_text=True, tolerance=0)
 def test_cmslabel():
     plt.style.use(hep.style.ROOT)
-    fig, ax = plt.subplots(figsize=(10, 5))
+    fig, ax = plt.subplots(figsize=(10, 10))
     hep.cms.cmstext("Blabla")
     return fig
