b972aa3ad423d15c862a459cb9330e01d5833de3
Restyled.io <commits@restyled.io>
2020-01-24 22:20:30 +0000

    Restyled by black

--- mplhep/atlas.py
+++ mplhep/atlas.py
@@ -1,22 +1,51 @@
 # Log styles
 from . import styles_atlas as style
 from . import label
-from . label import lumitext
+from .label import lumitext
+
 __all__ = [style, lumitext]
 
 
-def atlastext(text="", loc=0, ax=None,
-              fontname=None, fontsize=None, pad=0):
-    return label._exptext("ATLAS", text=text, loc=loc, ax=ax,
-                          fontname=None, fontsize=None, italic=(True, False),
-                          pad=pad)
+def atlastext(text="", loc=0, ax=None, fontname=None, fontsize=None, pad=0):
+    return label._exptext(
+        "ATLAS",
+        text=text,
+        loc=loc,
+        ax=ax,
+        fontname=None,
+        fontsize=None,
+        italic=(True, False),
+        pad=pad,
+    )
 
 
-def atlaslabel(ax=None, loc=0, data=False, paper=False, supplementary=False,
-               year=2017, lumi=None, llabel=None, rlabel=None, fontname=None,
-               fontsize=None, pad=0):
-    return label._explabel(ax=ax, loc=loc, data=data, paper=paper,
-                           supplementary=supplementary, year=year, lumi=lumi,
-                           llabel=llabel, rlabel=rlabel,
-                           fontname=fontname, fontsize=fontsize, pad=pad,
-                           exp="ATLAS", italic=(True, False))
+def atlaslabel(
+    ax=None,
+    loc=0,
+    data=False,
+    paper=False,
+    supplementary=False,
+    year=2017,
+    lumi=None,
+    llabel=None,
+    rlabel=None,
+    fontname=None,
+    fontsize=None,
+    pad=0,
+):
+    return label._explabel(
+        ax=ax,
+        loc=loc,
+        data=data,
+        paper=paper,
+        supplementary=supplementary,
+        year=year,
+        lumi=lumi,
+        llabel=llabel,
+        rlabel=rlabel,
+        fontname=fontname,
+        fontsize=fontsize,
+        pad=pad,
+        exp="ATLAS",
+        italic=(True, False),
+    )
--- mplhep/cms.py
+++ mplhep/cms.py
@@ -1,23 +1,52 @@
 # Log styles
 from . import styles_cms as style
 from . import label
-from . label import lumitext
+from .label import lumitext
+
 __all__ = [style, lumitext]
 
 
 # Experiment wrappers:
-def cmstext(text="", loc=0, ax=None,
-            fontname=None, fontsize=None, pad=0):
-    return label._exptext("CMS", text=text, loc=loc, ax=ax,
-                          fontname=None, fontsize=None, italic=(False, True),
-                          pad=pad)
+def cmstext(text="", loc=0, ax=None, fontname=None, fontsize=None, pad=0):
+    return label._exptext(
+        "CMS",
+        text=text,
+        loc=loc,
+        ax=ax,
+        fontname=None,
+        fontsize=None,
+        italic=(False, True),
+        pad=pad,
+    )
 
 
-def cmslabel(ax=None, loc=0, data=False, paper=False, supplementary=False,
-             year=2017, lumi=None, llabel=None, rlabel=None, fontname=None,
-             fontsize=None, pad=0):
-    return label._explabel(ax=ax, loc=loc, data=data, paper=paper,
-                           supplementary=supplementary, year=year, lumi=lumi,
-                           llabel=llabel, rlabel=rlabel,
-                           fontname=fontname, fontsize=fontsize, pad=pad,
-                           exp="CMS", italic=(False, True))
+def cmslabel(
+    ax=None,
+    loc=0,
+    data=False,
+    paper=False,
+    supplementary=False,
+    year=2017,
+    lumi=None,
+    llabel=None,
+    rlabel=None,
+    fontname=None,
+    fontsize=None,
+    pad=0,
+):
+    return label._explabel(
+        ax=ax,
+        loc=loc,
+        data=data,
+        paper=paper,
+        supplementary=supplementary,
+        year=year,
+        lumi=lumi,
+        llabel=llabel,
+        rlabel=rlabel,
+        fontname=fontname,
+        fontsize=fontsize,
+        pad=pad,
+        exp="CMS",
+        italic=(False, True),
+    )
