d1e03cd04a2a9ea45338e4d054dc7763a36ab6a6
andrzejnovak <novak5andrzej@gmail.com>
2020-01-31 14:33:40 +0100

    fix templating

--- mplhep/atlas.py
+++ mplhep/atlas.py
@@ -1,22 +1,22 @@
 # Log styles
 from . import styles_atlas as style
-from . import label
-from . label import lumitext
+from . import label as label_base
+from label_base import lumitext
 __all__ = [style, lumitext]
 
 
-def atlastext(text="", loc=0, ax=None,
-              fontname=None, fontsize=None, pad=0):
-    return label._exptext("ATLAS", text=text, loc=loc, ax=ax,
-                          fontname=None, fontsize=None, italic=(True, False),
-                          pad=pad)
+# Experiment wrappers:
+def atlastext(text="", **kwargs):
+    return label)_base._exptext("ATLAS", text=text, **kwargs, italic=(True, False))
 
 
-def atlaslabel(ax=None, loc=0, data=False, paper=False, supplementary=False,
-               year=2017, lumi=None, llabel=None, rlabel=None, fontname=None,
-               fontsize=None, pad=0):
-    return label._explabel(ax=ax, loc=loc, data=data, paper=paper,
-                           supplementary=supplementary, year=year, lumi=lumi,
-                           llabel=llabel, rlabel=rlabel,
-                           fontname=fontname, fontsize=fontsize, pad=pad,
-                           exp="ATLAS", italic=(True, False))
+def atlaslabel(**kwargs):
+    return label_base._explabel(**kwargs, exp="ATLAS", italic=(True, False))
+
+
+def text(**kwargs):
+    return atlastext(**kwargs)
+
+
+def label(**kwargs):
+    return atlaslabel(**kwargs)
