be089f84b541613e327d07333c1d352a07f2c6c8
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2020-11-11 00:58:56 +0100

    Fix autoformatted comment moved imports

--- src/mplhep/styles/__init__.py
+++ src/mplhep/styles/__init__.py
@@ -2,10 +2,9 @@ import sys
 
 from matplotlib.pyplot import style as plt_style
 
+# Short cut to all styles
 from .alice import ALICE
 from .atlas import ATLAS
-
-# Short cut to all styles
 from .cms import CMS, CMSTex, ROOT, ROOTTex
 from .lhcb import LHCb, LHCbTex, LHCb2, LHCbTex2
 
