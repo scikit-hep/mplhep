3080b36d693b5a7423212b0531880771e8b10b47
Jonas Eschle 'Mayou36 <mayou36@jonas.eschle.com>
2020-11-11 11:39:58 +0100

    Refactor deprecation dict

--- src/mplhep/_deprecate.py
+++ src/mplhep/_deprecate.py
@@ -1,5 +1,5 @@
-import collections
 import warnings
+
 
 # Adapted from:
 # https://github.com/scikit-hep/iminuit/blob/develop/iminuit/_deprecated.py
@@ -58,39 +58,110 @@ class deprecate_parameter(object):
         return decorated_func
 
 
-class deprecated_dict(collections.abc.MutableMapping):
+_NoArgumentGiven = object()
+
+
+class deprecated_dict(dict):
     """
     A dictionary that emits a deprecation warning. NOT a decorator!
     """
+    __slots__ = ("message", "_already_warned", "_warn_once", "_warning")  # no __dict__ - would be redundant
 
-    def __init__(self, *args, **kwargs):
-        self.store = dict()
-        message = kwargs.pop("message")
+    def __init__(self, *args, message: str = None, warn_once: bool = False, warning=DeprecationWarning, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._warn_once = warn_once
+        self._already_warned = False
+        self._warning = warning
         if message is not None:
             self.message = message
         else:
             self.message = "This dict is deprecated, please use another one instead"
 
     def __getitem__(self, key):
-        warnings.simplefilter("always", DeprecationWarning)
-        warnings.warn(self.message, category=DeprecationWarning, stacklevel=1)
-        warnings.simplefilter("default", DeprecationWarning)
-        return self.store[self.__keytransform__(key)]
+        self._warn_deprecation()
+        return super().__getitem__(key)
+
+    def _warn_deprecation(self):
+        if not (self._warn_once and self._already_warned):
+            warnings.warn(self.message, category=self._warning, stacklevel=1)
+            self._already_warned = True
 
     def __setitem__(self, key, value):
-        self.store[self.__keytransform__(key)] = value
+        self._warn_deprecation()
+        super().__setitem__(key, value)
 
     def __delitem__(self, key):
-        del self.store[self.__keytransform__(key)]
+        super().__delitem__(key)
 
     def __iter__(self):
-        warnings.simplefilter("always", DeprecationWarning)
-        warnings.warn(self.message, category=DeprecationWarning, stacklevel=1)
-        warnings.simplefilter("default", DeprecationWarning)
-        return iter(self.store)
+        self._warn_deprecation()
+        return super().__iter__()
 
     def __len__(self):
-        return len(self.store)
+        return super().__len__()
 
-    def __keytransform__(self, key):
-        return key
+    # Not included in the mapping
+    def get(self, k, default=None):
+        return super().get(k, default)
+
+    def setdefault(self, k, default=None):
+        return super().setdefault(k, default)
+
+    def pop(self, k, default=_NoArgumentGiven):
+        if default is _NoArgumentGiven:
+            return super().pop(k)
+        return super().pop(k, default)
+
+    def update(self, *args, **kwargs):
+        super().update(*args, **kwargs)
+
+    def __contains__(self, k):
+        self._warn_deprecation()
+        return super().__contains__(k)
+
+    def copy(self):  # don't delegate w/ super - dict.copy() -> dict
+        return type(self)(self)
+
+    @classmethod
+    def fromkeys(cls, keys, v=None):
+        return super().fromkeys(keys, v)
+
+    def __repr__(self):
+        return super().__repr__()
+
+# class LowerDict(dict):  # dicts take a mapping or iterable as their optional first argument
+#
+#     # def __init__(self, mapping=(), **kwargs):
+#     #     super(LowerDict, self).__init__(self._process_args(mapping, **kwargs))
+#     # def __getitem__(self, k):
+#     #     return super(LowerDict, self).__getitem__(ensure_lower(k))
+#     # def __setitem__(self, k, v):
+#     #     return super(LowerDict, self).__setitem__(ensure_lower(k), v)
+#     # def __delitem__(self, k):
+#     #     return super(LowerDict, self).__delitem__(ensure_lower(k))
+#     def get(self, k, default=None):
+#         return super(LowerDict, self).get(ensure_lower(k), default)
+#
+#     def setdefault(self, k, default=None):
+#         return super(LowerDict, self).setdefault(ensure_lower(k), default)
+#
+#     def pop(self, k, v=_RaiseKeyError):
+#         if v is _RaiseKeyError:
+#             return super(LowerDict, self).pop(ensure_lower(k))
+#         return super(LowerDict, self).pop(ensure_lower(k), v)
+#
+#     def update(self, mapping=(), **kwargs):
+#         super(LowerDict, self).update(self._process_args(mapping, **kwargs))
+#
+#     def __contains__(self, k):
+#         return super(LowerDict, self).__contains__(ensure_lower(k))
+#
+#     def copy(self):  # don't delegate w/ super - dict.copy() -> dict :(
+#         return type(self)(self)
+#
+#     @classmethod
+#     def fromkeys(cls, keys, v=None):
+#         return super(LowerDict, cls).fromkeys((ensure_lower(k) for k in keys), v)
+#
+#     def __repr__(self):
+#         return '{0}({1})'.format(type(self).__name__, super(LowerDict, self).__repr__())
