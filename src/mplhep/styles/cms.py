bd369389973e0d9a07fa82879a8a9b1d2f3b7040
Henry Schreiner <henryschreineriii@gmail.com>
2021-04-08 12:06:49 -0400

    chore: use from future import annotations

--- src/mplhep/styles/cms.py
+++ src/mplhep/styles/cms.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import matplotlib as mpl
 
 # PRL figsize / Elsevier figsize, Nature is somewhere in between
--- src/mplhep/utils.py
+++ src/mplhep/utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import enum
 from numbers import Real
 from typing import (
