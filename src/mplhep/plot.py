e7d025bed943fbbce060c9063a68287afb8350dc
Jeremy Wilkinson <77890459+jjhw3@users.noreply.github.com>
2023-10-03 15:22:02 +0100

    fix: _chunked_kwargs not expanding kwargs if they're tuples even if t… (#448)
    * fix: _chunked_kwargs not expanding kwargs if they're tuples even if the tuple isn't a valid mpl color
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/mplhep/plot.py
+++ src/mplhep/plot.py
@@ -306,8 +306,11 @@ def histplot(
     for kwarg in kwargs:
         # Check if iterable
         if iterable_not_string(kwargs[kwarg]):
-            # Check if tuple (can be used for colors)
-            if isinstance(kwargs[kwarg], tuple):
+            # Check if tuple of floats or ints (can be used for colors)
+            all_entries_numerical = all(
+                isinstance(x, int) or isinstance(x, float) for x in kwargs[kwarg]
+            )
+            if isinstance(kwargs[kwarg], tuple) and all_entries_numerical:
                 for i in range(len(_chunked_kwargs)):
                     _chunked_kwargs[i][kwarg] = kwargs[kwarg]
             else:
