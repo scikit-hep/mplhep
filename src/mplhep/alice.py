4399cb5acd1ff06ff810aba6eaf724d36910261b
Angus Hollands <goosey15@gmail.com>
2023-10-11 13:21:05 +0100

    fix: support older matplotlib

--- src/mplhep/alice.py
+++ src/mplhep/alice.py
@@ -2,7 +2,13 @@ from __future__ import annotations
 
 import inspect
 
-from matplotlib import _docstring as docstring
+import matplotlib
+from packaging.version import parse as parse_version
+
+if parse_version(matplotlib.__version__) < parse_version("3.8.0"):
+    from matplotlib import docstring
+else:
+    from matplotlib import _docstring as docstring
 
 import mplhep
 
