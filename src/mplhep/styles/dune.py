9ed9f0e9be34f39bfd2e637087eba213b89993ce
Jonas Eschle <jonas.eschle@cern.ch>
2025-08-26 16:08:09 +0200

    fix: re-add first color as black (#584)
    * fix: re-add first color as black
    
    * fix: adjust font size in dune.py and update .gitignore for generated files
    
    * test: update baseline

--- src/mplhep/styles/dune.py
+++ src/mplhep/styles/dune.py
@@ -33,7 +33,7 @@ DUNE1 = {
     "figure.autolayout": True,
     # Text properties
     "text.hinting_factor": 8,
-    "font.size": 24,
+    "font.size": 22,
     # Axes properties
     "axes.facecolor": "white",
     "axes.edgecolor": "black",
@@ -51,6 +51,7 @@ DUNE1 = {
     "axes.prop_cycle": cycler(
         "color",
         [
+            "#000000",  # Black
             "#D55E00",  # DUNE Orange (primary)
             "#56B4E9",  # DUNE Sky Blue (primary)
             "#E69F00",  # DUNE Yellow/Gold (primary)
